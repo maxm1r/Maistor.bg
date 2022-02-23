7231bb8f88a50266031cd51bcdd58b3b2740e604
Your Name <martololo16@gmail.com>
2022-02-23 14:13:00 +0200

    replaced BadRequestException with NotFoundException in some validations

--- src/main/java/com/finalproject/finalproject/service/PostService.java
+++ src/main/java/com/finalproject/finalproject/service/PostService.java
@@ -35,9 +35,9 @@ public class PostService {
     OfferRepository offerRepository;
 
     public PostResponseDTO createPost(int id, PostDTO postDTO) {
-        User user = userRepository.findById(id).orElseThrow(()-> new BadRequestException("User not found"));
-        Category category=categoryRepository.findByCategoryName(postDTO.getCategoryName())  .orElseThrow(()-> new BadRequestException("Category not found"));
-        City city = cityRepository.findByCityName(postDTO.getCityName()).orElseThrow(()-> new BadRequestException("City not found"));
+        User user = userRepository.findById(id).orElseThrow(()-> new NotFoundException("User not found"));
+        Category category=categoryRepository.findByCategoryName(postDTO.getCategoryName())  .orElseThrow(()-> new NotFoundException("Category not found"));
+        City city = cityRepository.findByCityName(postDTO.getCityName()).orElseThrow(()-> new NotFoundException("City not found"));
         if ( postDTO.getDescription().isEmpty() ||  postDTO.getDescription().isBlank()){
             throw new BadRequestException("Bad description");
         }
@@ -51,7 +51,7 @@ public class PostService {
         return  modelMapper.map(post, PostResponseDTO.class);
     }
     public PostResponseDTO deletePost(int id, int userId){
-        Post post = postRepository.findById(id).orElseThrow(()->new BadRequestException("Post not found"));
+        Post post = postRepository.findById(id).orElseThrow(()->new NotFoundException("Post not found"));
         if (post.getOwner().getId() != userId){
             throw new BadRequestException("User can't delete this post");
         }
@@ -64,19 +64,19 @@ public class PostService {
     }
 
     public PostResponseDTO editPost(PostDTO postDTO, int id) {
-        Post post = postRepository.findById(id).orElseThrow(()->new BadRequestException("Post not found"));
+        Post post = postRepository.findById(id).orElseThrow(()->new NotFoundException("Post not found"));
         if (postDTO.getDescription().isEmpty() || postDTO.getDescription().isBlank()){
             throw new BadRequestException("Bad Description");
         }
         post.setDescription(postDTO.getDescription());
-        post.setCategory(categoryRepository.findByCategoryName(postDTO.getCategoryName()).orElseThrow(()-> new BadRequestException("Category not found")));
-        post.setCity(cityRepository.findByCityName(postDTO.getCityName()).orElseThrow(()-> new BadRequestException("City not found")));
+        post.setCategory(categoryRepository.findByCategoryName(postDTO.getCategoryName()).orElseThrow(()-> new NotFoundException("Category not found")));
+        post.setCity(cityRepository.findByCityName(postDTO.getCityName()).orElseThrow(()-> new NotFoundException("City not found")));
         post = postRepository.save(post);
         return modelMapper.map(post,PostResponseDTO.class);
     }
     public PostResponseDTO acceptOffer(int postId, int offerId) {
-        Post post = postRepository.findById(postId).orElseThrow(()-> new BadRequestException("Post not found"));
-        Offer offer = offerRepository.findById(offerId).orElseThrow(()-> new BadRequestException("Offer not found"));
+        Post post = postRepository.findById(postId).orElseThrow(()-> new NotFoundException("Post not found"));
+        Offer offer = offerRepository.findById(offerId).orElseThrow(()-> new NotFoundException("Offer not found"));
         if (offer.getPost() != post){
             throw new BadRequestException("This offer doesn't belong to this post");
         }
